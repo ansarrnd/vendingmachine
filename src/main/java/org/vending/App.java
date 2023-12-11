d51320fa8006dee929aebdc467c73cbc35aa9636
Mohamed Ansardeen <ansar.rnd@gmail.com>
2023-12-11 14:21:17 +0800

    added APP

--- src/main/java/org/vending/App.java
+++ src/main/java/org/vending/App.java
@@ -1,10 +1,13 @@
 package org.vending;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
 
 /**
  * JavaFX App
@@ -12,13 +15,15 @@ import javafx.stage.Stage;
 public class App extends Application {
 
     @Override
-    public void start(Stage stage) {
-        var javaVersion = SystemInfo.javaVersion();
-        var javafxVersion = SystemInfo.javafxVersion();
+    public void start(Stage stage) throws IOException {
 
-        var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        var scene = new Scene(new StackPane(label), 640, 480);
-        stage.setScene(scene);
+        FXMLLoader loader = new FXMLLoader();
+        URL xmlUrl = getClass().getResource("/customer.fxml");
+        loader.setLocation(xmlUrl);
+        Parent root = loader.load();
+
+        stage.setTitle("Vending Machine");
+        stage.setScene(new Scene(root));
         stage.show();
     }
 
